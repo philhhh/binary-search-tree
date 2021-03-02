b6284de3cc99383fcaf10c17b3e99b0602606e60
philhhh <79876370+philhhh@users.noreply.github.com>
2021-03-02 11:23:18 -0800

    Update bst.js

--- bst.js
+++ bst.js
@@ -1,9 +1,5 @@
 /*
   This is the node class for a BST
-  It has three values
-  value - the value contained within the node
-  left - pointer to the left child node
-  right - pointer to the right child node
 */
 class Node {
   constructor(value){
