85c2117074eefa854d25d05f2e223e85c5c03e09
philhhh <79876370+philhhh@users.noreply.github.com>
2021-03-02 11:25:20 -0800

    Update bst.js

--- bst.js
+++ bst.js
@@ -1,10 +1,15 @@
 /*
   This is the node class for a BST
 */
-class Node {
+class Nod {
   constructor(value){
     this.value = value;
     this.left = null;
     this.right = null;
   }
+}
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
 }
